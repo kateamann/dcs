9526e81f1e0cd10f1ea8379c53bc66a9d1fa1dd4
Kate <hello@kateamann.com>
2020-01-13 14:53:23 +0000

    remove booking links from course overview

--- course-dates-loop.php
+++ course-dates-loop.php
@@ -16,22 +16,24 @@
     <li>
     	
         <?php echo $city; ?> (<?php echo $date; ?>)<strong>&nbsp;&middot;&nbsp;</strong><strong><?php echo $price; ?></strong><?php if( $link && $eventbrite ): ?>
+	        
+			<span class="eventlink">
+				<!-- Noscript content for added SEO -->
+				<noscript><a href="<?php echo esc_url($link); ?>" rel="noopener noreferrer" target="_blank"></noscript>
 
-	        <!-- Noscript content for added SEO -->
-			<noscript><a href="<?php echo esc_url($link); ?>" rel="noopener noreferrer" target="_blank"></noscript>
+				<strong>&nbsp;&middot;&nbsp;</strong><button class="eventbrite" id="eventbrite-widget-modal-trigger-<?php echo $eventbrite; ?>" type="button">Book <i class="fas fa-angle-double-right"></i></button>
+				<noscript></a>Buy Tickets on Eventbrite</noscript>
 
-			<strong>&nbsp;&middot;&nbsp;</strong><button class="eventbrite" id="eventbrite-widget-modal-trigger-<?php echo $eventbrite; ?>" type="button">Book <i class="fas fa-angle-double-right"></i></button>
-			<noscript></a>Buy Tickets on Eventbrite</noscript>
-
-			<script type="text/javascript">
-			    window.EBWidgets.createWidget({
-			        widgetType: 'checkout',
-			        eventId: '<?php echo $eventbrite; ?>',
-			        modal: true,
-			        modalTriggerElementId: 'eventbrite-widget-modal-trigger-<?php echo $eventbrite; ?>',
-			        onOrderComplete: exampleCallback
-			    });
-			</script>
+				<script type="text/javascript">
+				    window.EBWidgets.createWidget({
+				        widgetType: 'checkout',
+				        eventId: '<?php echo $eventbrite; ?>',
+				        modal: true,
+				        modalTriggerElementId: 'eventbrite-widget-modal-trigger-<?php echo $eventbrite; ?>',
+				        onOrderComplete: exampleCallback
+				    });
+				</script>
+			</span>
 		
 		<?php endif; ?>
 
